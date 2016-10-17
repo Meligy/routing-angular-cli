7271b54d094897703ded96bd456fc90310074fe2
Meligy <eng.meligy@gmail.com>
2016-10-17 12:43:21 +1100

    chore: delete code comment

--- src/app/bundled/bundled-routing.module.ts
+++ src/app/bundled/bundled-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-//import { BundledChildComponent } from './bundled-child/bundled-child.component';
 import { BundledComponent } from './bundled.component';
 
 export const routes: Routes = [
